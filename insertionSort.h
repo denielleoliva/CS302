43c2c0a3d3da1f5d178acc7a05691533a21b0e51
denielleoliva <denielle.oliva@gmail.com>
2020-09-26 00:49:02 -0700

    start of insertionSort

---
+++ insertionSort.h
@@ -0,0 +1,18 @@
+#ifndef INSERTIONSORTER_H_
+#define INSERTIONSORTER_H_
+
+using namespace std;
+
+template<typname DataType>
+class InsertionSort{
+
+public:
+
+    InsertionSort(const int maxSize);
+
+    int readFile(const std::string &file);
+
+    void sort(DataType )
+
+
+}
