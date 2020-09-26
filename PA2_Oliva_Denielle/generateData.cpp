48c4c2dcd1b665cb9d781a87040619d4546d1cf8
denielleoliva <denielle.oliva@gmail.com>
2020-09-26 00:16:17 -0700

    three test files

--- PA2_Oliva_Denielle/generateData.cpp
+++ PA2_Oliva_Denielle/generateData.cpp
@@ -17,22 +17,27 @@ int randomVal(int min, int max){
 int main(){
 
     int min, max, cnt;
+
+    min = 0;
+    max= 1000000;
+
     
     ofstream outFile;
 
-    outFile.open("testFile.txt");
+    outFile.open("testFile_1000.txt");
+
+    cnt = 1000;
 
     cout<<"***Random Number Generator***"<<endl<<endl;
 
-    cout<<"Min Value: ";
+    /*cout<<"Min Value: ";
     cin>>min;
 
     cout<<"Max Value: ";
     cin>>max;
 
     cout<<"How many values? ";
-    cin>>cnt;
-
+    cin>>cnt;*/
 
     for(int i = 0; i<cnt; i++){
         outFile<<randomVal(min, max);
@@ -40,5 +45,48 @@ int main(){
     }
 
     outFile.close();
+
+    outFile.open("testFile_10000.txt");
+
+    cnt = 10000;
+
+    /*cout<<"Min Value: ";
+    cin>>min;
+
+    cout<<"Max Value: ";
+    cin>>max;
+
+    cout<<"How many values? ";
+    cin>>cnt;*/
+
+    for(int i = 0; i<cnt; i++){
+        outFile<<randomVal(min, max);
+        outFile<<endl;
+    }
+
+    outFile.close();
+
+
+    outFile.open("testFile_100000.txt");
+
+    cnt = 100000;
+
+    /*cout<<"Min Value: ";
+    cin>>min;
+
+    cout<<"Max Value: ";
+    cin>>max;
+
+    cout<<"How many values? ";
+    cin>>cnt;*/
+
+    for(int i = 0; i<cnt; i++){
+        outFile<<randomVal(min, max);
+        outFile<<endl;
+    }
+
+    outFile.close();
+
+    cout<<"TEXT FILES GENERATED"<<endl;
 
 }
